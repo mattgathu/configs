39e32f99b2d29baea1b2e48b4b5fdbbf21e378fa
Matt Gathu <mattgathu@gmail.com>
2017-02-28 12:06:34 +0300

    updated script

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ brew cask install --caskroom=/Applications dropbox onedrive  skype slack
 echo "Installing your virtual machines"
 brew cask  install --caskroom=/Applications virtualbox vagrant vagrant-manager
 echo "Installing ansible git"
-brew cask install --caskroom=/Applications  ansible git vim python python3
+brew install ansible git vim python python3
 
 echo "Install docker and docker tools"
 brew cask install docker-toolbox docker docker-compose 
 
 echo "Installing AWS clients"
-brew cask install --caskroom=/Applications  awscli aws-elasticbeanstalk
+brew install awscli aws-elasticbeanstalk
 
 echo "Installing Others"
 brew cask install --caskroom=/Applications  pritunl
